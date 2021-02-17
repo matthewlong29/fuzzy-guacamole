f0e8ecdaa621c9647826c6135b4958808156dc90
Matthew Long <long.matthew29@gmail.com>
2021-02-16 20:59:53 -0500

    added new routes

--- src/app/fuzzy-guacamole-routing.module.ts
+++ src/app/fuzzy-guacamole-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { ExploreComponent } from './explore/explore.component';
+import { MessagesComponent } from './messages/messages.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'explore',
+    component: ExploreComponent
+  },
+  {
+    path: 'notifications',
+    component: NotificationsComponent
+  },
+  {
+    path: 'messages',
+    component: MessagesComponent
+  },
+  {
+    path: 'bookmarks',
+    component: BookmarksComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: '',
+    redirectTo: '/profile',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/fuzzy-guacamole.module.ts
+++ src/app/fuzzy-guacamole.module.ts
@@ -6,6 +6,11 @@ import { AsideComponent } from './layout/aside/aside.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { MainComponent } from './layout/main/main.component';
 import { NavigationComponent } from './layout/navigation/navigation.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { ExploreComponent } from './explore/explore.component';
+import { MessagesComponent } from './messages/messages.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,12 @@ import { NavigationComponent } from './layout/navigation/navigation.component';
     HeaderComponent,
     MainComponent,
     AsideComponent,
-    FuzzyGuacamoleComponent
+    FuzzyGuacamoleComponent,
+    ProfileComponent,
+    BookmarksComponent,
+    ExploreComponent,
+    MessagesComponent,
+    NotificationsComponent
   ],
   imports: [
     BrowserModule,
